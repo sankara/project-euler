7536e040dac2d8efdeccf8bd80e04f3ea46cf856
Sankara Rameswaran <sankara.rameswaran@gmail.com>
2009-07-28 11:14:13 +0530

    Ignore funky stuff.

--- src/main/clojure/euler/problem-1.clj
+++ src/main/clojure/euler/problem-1.clj
@@ -1,4 +1,8 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;; Problem 1 ;;;;;;;;;;;;;
+; Sum of numbers divisible by 3 or 5
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (ns euler.problem-1)
-(println (apply +
-  (filter #(or (zero? (rem % 3)) (zero? (rem % 5)))
-    (range 1000))))
+(println (apply + (filter #(or (zero? (rem % 3)) (zero? (rem % 5)))
+  (range 1000))))
