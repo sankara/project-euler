65ae56d906e67a3747301ed025daa609cae0a2fe
Sankara Rameswaran <sankara.rameswaran@gmail.com>
2009-08-18 16:53:01 +0530

    Problem 9 & 10

--- src/main/clojure/euler/problem-9.clj
+++ src/main/clojure/euler/problem-9.clj
@@ -1,3 +1,4 @@
-(ns euler.problem-8)
-(use 'clojure.contrib.lazy-seqs)
-(println (time (reduce + (take-while #(< % 2000000) primes))))
+(ns euler.problem-9)
+(dorun (for [a (range 1 1000) b (range a 1000)]
+  (let [c (- 1000 (+ a b))]
+    (if (= (+ (* a a) (* b b)) (* c c)) (println a b c)))))
