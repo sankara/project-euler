888e39575bc2c16cdb510d0259a2c1f6feb61b79
Sankara Rameswaran <sankara.rameswaran@gmail.com>
2009-08-18 17:08:51 +0530

    Minor edit.

--- src/main/clojure/euler/problem-9.clj
+++ src/main/clojure/euler/problem-9.clj
@@ -1,4 +1,7 @@
 (ns euler.problem-9)
-(dorun (for [a (range 1 1000) b (range a 1000)]
+(time (dorun (for [a (range 1 1000) b (range a 1000)]
   (let [c (- 1000 (+ a b))]
-    (if (= (+ (* a a) (* b b)) (* c c)) (println a b c)))))
+    (if (= (+ (* a a) (* b b)) (* c c)) (println a b c))))))
+
+;200 375 425
+;"Elapsed time: 1176.72584 msecs"
