9e157f196a55ffbfe47d8500125367cc6c0464ee
Sankara Rameswaran <sankara.rameswaran@gmail.com>
2010-03-13 16:24:25 +0530

    32. Find the sum of all numbers that can be written as pandigital products.

---
+++ src/main/clojure/euler/32-pandigital-product.clj
@@ -0,0 +1,18 @@
+(ns euler.32
+  (:use clojure.contrib.combinatorics))
+
+(defn pandigital? [s]
+  (and
+    (= 9 (count s))
+    (= #{\1 \2 \3 \4 \5 \6 \7 \8 \9} (set s))))
+
+(defn main []
+  (reduce + (distinct (map last
+    (filter #(pandigital? (str (first %) (second %) (last %)))
+      (for [i (range 1 99) j (range 1 9999)]
+        [i j (* i j)]))))))
+
+(println (time (main)))
+
+;"Elapsed time: 3432.904263 msecs"
+;45228
