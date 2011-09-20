9c4614a5ca75b5d3be12c3f217873d7edbf1436d
Sankara Rameswaran <sankara@bankbazaar.com>
2011-09-20 21:37:48 +0530

    Upgrading clojure

--- project.clj
+++ project.clj
@@ -2,6 +2,5 @@
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.3.0-RC0"]]
+  :dev-dependencies [[swank-clojure "1.3.2" :exclusions [org.clojure/clojure]]])
