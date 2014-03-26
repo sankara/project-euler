93e6384d8b8e03b495990a11e6418a84afb86c2a
Sankara Rameswaran <sankara.rameswaran@gmail.com>
2014-03-26 15:04:43 -0700

    Adding rethinkdb

--- project.clj
+++ project.clj
@@ -2,9 +2,12 @@
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [
+     [org.clojure/clojure "1.6.0"]
 		 [org.clojure/tools.nrepl "0.2.3"]
-		 [lein-light-nrepl "0.0.17"]]
+		 [lein-light-nrepl "0.0.17"]
+     [revise "0.0.6"]
+  ]
   :repl-options {
 	:host "dev.sankara.me"
 	:port 9090
